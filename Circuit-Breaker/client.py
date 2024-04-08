903300aac9192e008d5bc65beda5c333f0ce1962
Minh <phamquangminh1659@gmail.com>
2024-04-08 19:12:50 +0700

    Add logging and accelerated circuit breaking features

--- Circuit-Breaker/client.py
+++ Circuit-Breaker/client.py
@@ -3,6 +3,7 @@ import requests
 import time
 from http.client import responses
 from threading import Timer
+import logging
 
 
 class CircuitBreakerStateEnum(Enum):
@@ -11,17 +12,21 @@ class CircuitBreakerStateEnum(Enum):
     OPEN = 2
 
 
+class CircuitBreakerOpenException(Exception):
+    pass
+
+
 class CircuitBreakerStateStore:
     def __init__(self) -> None:
         self.state = CircuitBreakerStateEnum.CLOSE
-        self.last_state_changed_date_utc = time.time_ns()
+        self.last_state_changed_date_utc = time.time()
         self.last_exception = 0
         # for the live demo
         self.state_changed = False
 
     def trip(self, exception) -> None:
         self.last_exception = exception
-        self.last_state_changed_date_utc = time.time_ns()
+        self.last_state_changed_date_utc = time.time()
         self.state = CircuitBreakerStateEnum.OPEN
         # for the live demo
         self.state_changed = True
@@ -49,12 +54,15 @@ class CircuitBreakerStateStore:
 class CircuitBreaker:
     def __init__(self) -> None:
         self.state_store = CircuitBreakerStateStore()
-        self.open_to_half_open_wait_time = 5 * 10**9  # 5 seconds
+        self.open_to_half_open_wait_time = 5  # seconds
+        self.default_open_state_timeout = 5  # seconds
+        self.too_much_error_open_state_timeout = 8  # seconds
+        self.overload_error_open_state_timeout = 10  # seconds
         self.failure_counter = 0
         self.success_counter = 0
         self.failure_threshold = 5
         self.success_threshold = 5
-        self.failure_interval = 10  # 10 seconds
+        self.failure_interval = 10  # seconds
         self.reset_timer = None
         self.start_failure_count_reset_timer()
 
@@ -77,26 +85,42 @@ class CircuitBreaker:
     def is_half_open(self) -> bool:
         return self.state_store.is_half_open()
 
-    def action(self) -> int:
-        request = requests.get("http://127.0.0.1:5000")
-        return request.status_code
+    def action(self) -> requests.Response:
+        response = requests.get("http://127.0.0.1:5000")
+        return response
 
     def execute_action(self):
         if self.is_open():
             if (
                 self.state_store.last_state_changed_date_utc
                 + self.open_to_half_open_wait_time
-                < time.time_ns()
+                < time.time()
             ):
                 self.state_store.half_open()
                 print("Circuit Breaker has entered HALF-OPEN state")
                 self.success_counter = 0
             else:
-                raise Exception("Request not allowed (Circuit Breaker is Open).")
+                raise CircuitBreakerOpenException(
+                    "Request not allowed (Circuit Breaker is Open)."
+                )
         if self.is_half_open():
-            status = self.action()
+            response = self.action()
+            status = response.status_code
             if status != 200:
-                self.track_exception(status)
+                # if the service sends the estimated delay time
+                if "Retry-After" in response.headers:
+                    retry_after = int(response.headers["Retry-After"])
+                    print(f"Retry-After detected: {retry_after}")
+                    # trip to open state immediately and set open state's timeout to retry_after
+                    self.track_exception(status, retry_after)
+                elif status == 429:
+                    # trip to open state immediately and set open state's timeout to the specified value for 429 error
+                    self.track_exception(status, self.too_much_error_open_state_timeout)
+                elif status == 503:
+                    # trip to open state immediately and set open state's timeout to the specified value for 503 error
+                    self.track_exception(status, self.overload_error_open_state_timeout)
+                else:  # less severe error (e.g. timeout error)
+                    self.track_exception(status, self.default_open_state_timeout)
                 raise Exception(f"Error: {status} {responses[status]}")
             else:
                 self.success_counter += 1
@@ -106,17 +130,54 @@ class CircuitBreaker:
                     self.start_failure_count_reset_timer()
                 return
 
-        status = self.action()
+        response = self.action()
+        status = response.status_code
         if status != 200:
-            self.failure_counter += 1
-            if self.failure_counter == self.failure_threshold:
-                self.track_exception(status)
+            # if the service sends the estimated delay time
+            if "Retry-After" in response.headers:
+                retry_after = int(response.headers["Retry-After"])
+                print(f"Retry-After detected: {retry_after}")
+                # trip to open state immediately and set open state's timeout to retry_after
+                self.track_exception(status, retry_after)
+            elif status == 429:
+                # trip to open state immediately and set open state's timeout to the specified value for 429 error
+                self.track_exception(status, self.too_much_error_open_state_timeout)
+            elif status == 503:
+                # trip to open state immediately and set open state's timeout to the specified value for 503 error
+                self.track_exception(status, self.overload_error_open_state_timeout)
+            else:  # less severe error (e.g. timeout error)
+                self.failure_counter += 1
+                if self.failure_counter == self.failure_threshold:
+                    self.track_exception(status, self.default_open_state_timeout)
             raise Exception(f"Error: {status} {responses[status]}")
-        # else:
-        # self.failure_counter = 0
 
-    def track_exception(self, status) -> None:
+    def track_exception(self, status, timeout) -> None:
         self.state_store.trip(status)
+        self.open_to_half_open_wait_time = timeout
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("circuit_breaker")
+logger.propagate = False
+
+# Set up file handler for logging requests
+handler = logging.FileHandler("circuit_breaker.log")
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+# Add handler to the logger
+logger.addHandler(handler)
+
+
+# Function to log success messages
+def log_success(message):
+    logger.info(message)
+
+
+# Function to log error messages
+def log_error(message):
+    logger.error(message)
 
 
 if __name__ == "__main__":
@@ -125,8 +186,12 @@ if __name__ == "__main__":
         try:
             circuit_breaker.execute_action()
             print("Request successfully: 200 OK")
+            log_success("Successful request.")
         except Exception as e:
             print(e)
+            if not isinstance(e, CircuitBreakerOpenException):
+                error_message = f"Failed requests: {e}"
+                log_error(error_message)
 
         if circuit_breaker.state_store.state_changed:
             if circuit_breaker.is_close():
--- Circuit-Breaker/server.py
+++ Circuit-Breaker/server.py
@@ -1,36 +1,41 @@
-from flask import Flask, abort
+from flask import Flask, make_response
 import threading
+from http.client import responses
 
 app = Flask(__name__)
 
 status = 200
+retry_after = 0
 
 
 def test():
     global status
+    global retry_after
     while True:
-        try:
-            e = input()
-        except Exception:
-            return
+        numbers = [int(user_input) for user_input in input().split()]
+        status = numbers[0]
+        retry_after = numbers[1] if len(numbers) > 1 else 0
 
-        if e == "504":  # gateway timeout
-            status = 504
-        elif e == "429":  # client sending too many requests
-            status = 429
-        elif e == "503":  # service temporarily unavailable
-            status = 503
-        elif e == "200":
-            status = 200
+        # if e == "504":  # gateway timeout
+        #     status = 504
+        # elif e == "429":  # client sending too many requests
+        #     status = 429
+        # elif e == "503":  # service temporarily unavailable
+        #     status = 503
+        # elif e == "200":
+        #     status = 200
 
-        print(status)
+        print(status, retry_after)
 
 
 @app.route("/")
 def index():
     global status
     if status != 200:
-        abort(status)
+        response = make_response(responses[status], status)
+        if retry_after > 0:
+            response.headers["Retry-After"] = retry_after  # The value is in seconds
+        return response
     return "Hello"
 
 
